1f9d8bd2993e7ba521ef0f9f7008101b7f1099e6
Islam <rifat.islam19@mbpr1315-1787.trinity.nyc.ny.us>
2018-11-29 14:17:46 -0500

    edit

--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -10,7 +10,7 @@ if(player["games"]>0){
   var new_browser = JSON.parse(localStorage.getItem("browser"));
   updateMessage("games_played", "Games played: " + new_player["games"]);
   updateMessage("total_wins", "Total wins: " + new_player["wins"]);
-  if(parseInt(player["wins"])>0||player["losses"]>0){
+  if(parseInt(player["wins"])>0||parseInt(player["losses"])>0){
     updateMessage("win_loss_ratio", "Win/Loss ratio: " + parseInt(new_player["wins"]) + " - " + parseInt(new_player["losses"]));
   }
   updateMessage("player_stats", "Player throws: rock - " + ((parseInt(new_player["rock"])/parseInt(new_player["games"]))*100).toFixed(0) +"%, scissors - " + ((parseInt(new_player["scissors"])/parseInt(new_player["games"]))*100).toFixed(0) + "%, paper - " + ((parseInt(new_player["paper"])/parseInt(new_player["games"]))*100).toFixed(0) + "%");
